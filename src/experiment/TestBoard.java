665e0d1a967b2d68f114edc871f90fffac6ad7e1
Brendan <buebelhoer@mymail.mines.edu>
2021-02-25 19:12:04 -0700

    added more tests for calculation of movement, and added more comments

--- src/experiment/TestBoard.java
+++ src/experiment/TestBoard.java
@@ -4,24 +4,24 @@ import java.util.*;
 
 public class TestBoard {
 	
-	private static final int NUM_ROWS = 4;
-	private static final int NUM_COLS = 4;
+	private static final int NUM_ROWS = 4; // number of rows in the board
+	private static final int NUM_COLS = 4; // number of coloumn in the board
 	
-	private TestBoardCell[][] board;
-	private Set<TestBoardCell> targets;
+	private TestBoardCell[][] board;//stores the board itself
+	private Set<TestBoardCell> targets;//stores the movable cells
 	
 	public TestBoard() {
-		board = new TestBoardCell[NUM_ROWS][NUM_COLS];
+		board = new TestBoardCell[NUM_ROWS][NUM_COLS]; //inits the board
 		for (int i = 0; i < NUM_ROWS; i++) {
 			for (int j = 0; j < NUM_COLS; j++) {
-				board[i][j] = new TestBoardCell(i,j);
+				board[i][j] = new TestBoardCell(i,j); // fills in the cells with their cords
 			}
 		}
 		targets = new HashSet<TestBoardCell>();
 	}
 
 	public void calcTargets( TestBoardCell startCell, int pathlength) {
-		
+		//will contain the pathing function
 	}
 	
 	public Set<TestBoardCell> getTargets() {
--- src/tests/BoardTestsExp.java
+++ src/tests/BoardTestsExp.java
@@ -52,28 +52,28 @@ public class BoardTestsExp {
 	}
 	
 	@Test
-	public void testCalcsTL() {
-		board.calcTargets(board.getCell(0, 0), 2);
+	public void testCalcsTL() { // starts the player on the top left
+		board.calcTargets(board.getCell(0, 0), 2); // 2 movement
 		Set<TestBoardCell> targets = board.getTargets();
 		assertTrue(targets.contains(board.getCell(0, 2)));
 		assertTrue(targets.contains(board.getCell(1, 1)));
 		assertTrue(targets.contains(board.getCell(2, 0)));
-		assertEquals(3, targets.size());
+		assertEquals(3, targets.size()); //should produce 3 moves
 	}
 	
 	@Test
-	public void testCalcsRB() {
-		board.calcTargets(board.getCell(3, 3), 2);
+	public void testCalcsRB() { // starts the player on the bottom right
+		board.calcTargets(board.getCell(3, 3), 2);// 2 movement
 		Set<TestBoardCell> targets = board.getTargets();
 		assertTrue(targets.contains(board.getCell(1, 3)));
 		assertTrue(targets.contains(board.getCell(2, 2)));
 		assertTrue(targets.contains(board.getCell(3, 1)));
-		assertEquals(3, targets.size());
+		assertEquals(3, targets.size()); //should produce 3 moves
 	}
 	
 	@Test
-	public void testCalcsMid() {
-		board.calcTargets(board.getCell(1, 1), 3);
+	public void testCalcsMid() { // starts the player in the middle
+		board.calcTargets(board.getCell(1, 1), 3); // starts in the middle, 3 movement
 		Set<TestBoardCell> targets = board.getTargets();
 		assertTrue(targets.contains(board.getCell(0, 1)));
 		assertTrue(targets.contains(board.getCell(0, 3)));
@@ -83,6 +83,44 @@ public class BoardTestsExp {
 		assertTrue(targets.contains(board.getCell(2, 3)));
 		assertTrue(targets.contains(board.getCell(3, 0)));
 		assertTrue(targets.contains(board.getCell(3, 2)));
-		assertEquals(3, targets.size());
+		assertEquals(8, targets.size()); //should produce 8 moves
+	}
+	
+	@Test
+	public void testCalcsDoor() {
+		board.getCell(0, 1).setRoom(true); // sets a door directly right of the starting point
+		board.calcTargets(board.getCell(1, 1), 2); //starts top left, 2 movement
+		Set<TestBoardCell> targets = board.getTargets();
+		assertTrue(targets.contains(board.getCell(0, 1))); // should stop at the door
+		assertTrue(targets.contains(board.getCell(1, 1)));
+		assertTrue(targets.contains(board.getCell(2, 0)));
+		assertEquals(3, targets.size()); //should produce 3 moves
+	}
+	
+	@Test
+	public void testCalcOccupied() {
+		board.getCell(1, 0).setOccupied(true);//sets the spot directly below the player to occupied
+		board.calcTargets(board.getCell(1, 1), 2); //starts top left, 2 movement
+		Set<TestBoardCell> targets = board.getTargets();
+		assertTrue(targets.contains(board.getCell(0, 2)));
+		assertTrue(targets.contains(board.getCell(1, 1)));
+		assertEquals(2, targets.size()); //should produce 2 moves
+	}
+	
+	@Test
+	public void testCalcDoorOccupiedMid() { // biggest test, with a door to the
+		board.getCell(3, 1).setOccupied(true);//sets the spot 2 below the player to occupied
+		board.getCell(0, 3).setRoom(true); //sets the top right spot to be a door
+		board.calcTargets(board.getCell(1, 1), 4); //starts top left, with 4 movement
+		Set<TestBoardCell> targets = board.getTargets();
+		assertTrue(targets.contains(board.getCell(0, 0)));
+		assertTrue(targets.contains(board.getCell(0, 2)));
+		assertTrue(targets.contains(board.getCell(1, 3)));
+		assertTrue(targets.contains(board.getCell(2, 0)));
+		assertTrue(targets.contains(board.getCell(2, 2)));
+		assertTrue(targets.contains(board.getCell(3, 3)));
+		assertTrue(targets.contains(board.getCell(0, 3))); //should stop at the door
+		assertEquals(7, targets.size()); //should produce 7 moves
 	}
 }
+
