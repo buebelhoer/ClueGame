0a0e7f518c3aeebf93893e8e611a43af9e307ed3
Brendan <buebelhoer@mymail.mines.edu>
2021-02-28 20:20:50 -0700

    implemented room class

--- src/clueGame/Room.java
+++ src/clueGame/Room.java
@@ -2,4 +2,17 @@ package clueGame;
 
 public class Room {
 
+	String name;
+	BoardCell centerCell;
+	BoardCell labelCell;
+	
+	public Room(String name, BoardCell centerCell, BoardCell labelCell) {
+		super();
+		this.name = name;
+		this.centerCell = centerCell;
+		this.labelCell = labelCell;
+	}
+	
+	
+	
 }
