1d5008096efc6c64322f7f6548e702237cda0ff5
bMorgan01 <ben.morgan5000@gmail.com>
2021-03-16 17:59:37 -0600

    i need the setup plz, must push first

--- src/tests/GameSetupTests.java
+++ src/tests/GameSetupTests.java
@@ -1,5 +1,7 @@
 package tests;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
@@ -12,7 +14,7 @@ public class GameSetupTests {
 	
 	@Test
 	public void testPlayersLoaded() {
-		
+		assertEquals(6, board.getPlayerList().length());
 	}
 	
 	@Test
