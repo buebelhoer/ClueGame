d01e6faf782dd8f89536fed1de64c4fe7f94493f
Brendan <buebelhoer@mymail.mines.edu>
2021-03-16 17:58:14 -0600

    added test setup

--- src/tests/GameSetupTests.java
+++ src/tests/GameSetupTests.java
@@ -3,11 +3,15 @@ package tests;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import clueGame.Board;
+
 public class GameSetupTests {
 
 	@BeforeEach
 	public void setupTests(){
-		
+		Board board = Board.getInstance();
+		board.setConfigFiles("ClueLayout.csv", "ClueSetup.txt");
+		board.initialize();
 	}
 	
 	@Test
