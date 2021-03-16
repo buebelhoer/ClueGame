8ed0d47b792a7a4cc5d00b1be65b91c34c8f53fc
Brendan <buebelhoer@mymail.mines.edu>
2021-03-16 17:23:44 -0600

    implemented Card

--- src/clueGame/Card.java
+++ src/clueGame/Card.java
@@ -2,4 +2,23 @@ package clueGame;
 
 public class Card {
 
+	private String cardName;
+	private CardType cardType;
+	
+	
+	public boolean equals(Card target) {
+		return target.getCardName() == this.cardName;
+	}
+
+
+	public String getCardName() {
+		return cardName;
+	}
+
+
+	public CardType getCardType() {
+		return cardType;
+	}
+	
+	
 }
