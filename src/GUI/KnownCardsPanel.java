7b98b89599e2d9d00095b52401f392b2328c8358
bMorgan01 <ben.morgan5000@gmail.com>
2021-04-05 15:55:03 -0600

    added known card panel class

---
+++ src/GUI/KnownCardsPanel.java
@@ -0,0 +1,37 @@
+package GUI;
+
+import java.awt.Color;
+import java.awt.GridLayout;
+
+import javax.swing.BorderFactory;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+
+import clueGame.ComputerPlayer;
+
+public class KnownCardsPanel extends JPanel {
+	/**
+	 * Constructor for the panel, it does 90% of the work
+	 */
+	public KnownCardsPanel()  {
+	
+	}
+	
+	/**
+	 * Main to test the panel
+	 * 
+	 * @param args
+	 */
+	public static void main(String[] args) {		
+		KnownCardsPanel panel = new KnownCardsPanel();  // create the panel
+		panel.setLayout(new GridLayout(3, 1));
+		JFrame frame = new JFrame();  // create the frame 
+		frame.setContentPane(panel); // put the panel in the frame
+		frame.setSize(750, 180);  // size the frame
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); // allow it to close
+		frame.setVisible(true); // make it visible
+	}
+}
