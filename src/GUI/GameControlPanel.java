3bc0ac4cf149dfdd4b5ec233642acff7f86d1091
bMorgan01 <ben.morgan5000@gmail.com>
2021-04-05 14:54:32 -0600

    added top part of panel

--- src/GUI/GameControlPanel.java
+++ src/GUI/GameControlPanel.java
@@ -1,9 +1,13 @@
 package GUI;
 
 import java.awt.Color;
+import java.awt.GridLayout;
 
+import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.JTextField;
 
 import clueGame.ComputerPlayer;
 
@@ -14,7 +18,31 @@ public class GameControlPanel extends JPanel {
 	 * Constructor for the panel, it does 90% of the work
 	 */
 	public GameControlPanel()  {
+		JPanel turnPanel = new JPanel(new GridLayout(1, 4));
 		
+		JPanel playerPanel = new JPanel(new GridLayout(2, 1));
+		JLabel whoseLabel = new JLabel("Whose turn?");
+		JTextField nameField = new JTextField();
+		nameField.setEditable(false);
+		
+		playerPanel.add(whoseLabel);
+		playerPanel.add(nameField);
+		
+		JPanel rollPanel = new JPanel();
+		JLabel rollLabel = new JLabel("Roll:");
+		JTextField rollField = new JTextField();
+		rollField.setEditable(false);
+		
+		rollPanel.add(rollLabel);
+		rollPanel.add(rollField);
+		
+		JButton accuseButton = new JButton("Make Accusation");
+		JButton nextButton = new JButton("NEXT!");
+		
+		turnPanel.add(playerPanel);
+		turnPanel.add(rollPanel);
+		turnPanel.add(accuseButton);
+		turnPanel.add(nextButton);
 	
 		JPanel guessPanel = new JPanel();
 		JPanel 
