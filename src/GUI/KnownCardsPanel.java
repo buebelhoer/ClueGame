272f7a0d680c4c2f8cabe8d96fc52e9155fd661c
Buebelhoer <buebelhoer@mymail.mines.edu>
2021-04-05 17:32:39 -0600

    added none card logic

--- src/GUI/KnownCardsPanel.java
+++ src/GUI/KnownCardsPanel.java
@@ -1,6 +1,7 @@
 package GUI;
 
 import java.awt.Color;
+import java.awt.Font;
 import java.awt.GridLayout;
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -8,6 +9,7 @@ import java.util.Map;
 import java.util.Map.Entry;
 
 import javax.swing.BorderFactory;
+import javax.swing.BoxLayout;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
@@ -70,6 +72,7 @@ public class KnownCardsPanel extends JPanel {
 				}
 			}
 		}
+	
 	}
 	
 	/**
@@ -77,11 +80,18 @@ public class KnownCardsPanel extends JPanel {
 	 * 
 	 * @param args
 	 */
-	public static void main(String[] args) {		
-		KnownCardsPanel panel = new KnownCardsPanel();  // create the panel
+	public static void main(String[] args) {
+		
+		ArrayList<Card> hand = new ArrayList<>();
+		hand.add(new Card("handTestweapon", CardType.WEAPON));
+		hand.add(new Card("handTestweapon2", CardType.WEAPON));
+		
+		HashMap<Player, ArrayList<Card>> seenCards = new HashMap<>();
+		
+		KnownCardsPanel panel = new KnownCardsPanel(hand, seenCards);  // create the panel
 		JFrame frame = new JFrame();  // create the frame 
 		frame.setContentPane(panel); // put the panel in the frame
-		frame.setSize(750, 180);  // size the frame
+		frame.setSize(180,650);  // size the frame
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); // allow it to close
 		frame.setVisible(true); // make it visible
 	}
@@ -89,36 +99,45 @@ public class KnownCardsPanel extends JPanel {
 	//class that forms the panel for each of the three types of cards
 	private class CardTypePanel extends JPanel {
 		
-		CardPanel handCardPanel;
-		CardPanel seenCardPanel;
+		private CardPanel handCardPanel;
+		private CardPanel seenCardPanel;
 		
 		public CardTypePanel(String name) {
 			super();
 			setBorder(BorderFactory.createTitledBorder(BorderFactory.createLineBorder(Color.black), name));
 			setLayout(new GridLayout(2, 1));
 			
+			//create a bold font
+			Font font = this.getFont();
+			font = font.deriveFont(Font.BOLD,font.getSize());
+			
+			
 			//creats the panel for cards in the players hand
 			JPanel handPanel = new JPanel();
 			handPanel.setLayout(new GridLayout(2,1));
 			
 			//adds label of the hand panel
-			handPanel.add(new JTextField("In Hand:"));
+			handPanel.add(new JLabel("In Hand:"));
 			
 			//creates the cardpanel that stores the card in the hand
 			handCardPanel = new CardPanel();
 			handPanel.add(handCardPanel);
+			System.out.println("hand panel added");
 			
 			JPanel seenPanel = new JPanel();
 			seenPanel.setLayout(new GridLayout(2,1));
 			
 			//adds label of seen panel
-			seenPanel.add(new JTextField("Seen:"));
+			seenPanel.add(new JLabel("Seen:"));
 			
 			//creates the cardpanel that stores the cards that have been seen
 			
 			seenCardPanel = new CardPanel();
 			seenPanel.add(handCardPanel);
 			
+			//adds the hand and seen panels to the overall panel
+			add(handPanel);
+			add(seenPanel);
 			
 		}
 		
@@ -133,14 +152,29 @@ public class KnownCardsPanel extends JPanel {
 	
 	//class that forms a panel and shows all the cards in a particular category(Type and seen status)
 	private class CardPanel extends JPanel {
+		private JTextField noneCard;
+		
 		CardPanel() {
 			super();
+			System.out.println("card panel created");
+			setLayout(new BoxLayout(this, BoxLayout.Y_AXIS));
+			noneCard = new JTextField("None");
+			noneCard.setBorder(BorderFactory.createLineBorder(Color.black));
+			noneCard.setBackground(Color.white);
+			noneCard.setEditable(false);
+			noneCard.setHorizontalAlignment(JTextField.CENTER);
+			add(noneCard);
+			
 		}
 		
 		public void addCard(Color color, String cardName) {
+			this.remove(noneCard);
+			
 			JTextField cardField = new JTextField(cardName);
 			cardField.setBorder(BorderFactory.createLineBorder(Color.black));
 			cardField.setBackground(color);
+			cardField.setEditable(false);
+			cardField.setHorizontalAlignment(JTextField.CENTER);
 			add(cardField);
 		}
 	}
